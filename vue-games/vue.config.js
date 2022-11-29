1b3d63e0e7d60ab329f4aaa3fea1ea3a43707bd3
bgroveben <bgroveben@gmail.com>
2022-11-28 19:30:06 -0800

    Set up Vue to run inside Django app

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: 'http://localhost:8080', // base URL
+  outputDir: '../static/dist', // where files will be output
+  indexPath: '../../templates/_base_vue.html', // path for the index file
+
+  configureWebpack: {
+    devServer: {
+      devMiddleware: {
+        writeToDisk: true
+      }
+    }
+  }
 })
