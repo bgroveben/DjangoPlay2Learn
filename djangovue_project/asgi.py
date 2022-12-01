a65ae327505310725e74f2b0d8d1d88b59764ca3
bgroveben <bgroveben@gmail.com>
2022-12-01 11:07:37 -0800

    Install django app

--- djangovue_project/asgi.py
+++ djangovue_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoplay2learn project.
+ASGI config for djangovue_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoplay2learn.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangovue_project.settings')
 
 application = get_asgi_application()
--- djangovue_project/wsgi.py
+++ djangovue_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoplay2learn project.
+WSGI config for djangovue_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoplay2learn.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangovue_project.settings')
 
 application = get_wsgi_application()
