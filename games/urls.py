cd2aa10fce3ddf5c46b876fa9628a585b2b36e1e
bgroveben <bgroveben@gmail.com>
2022-12-10 18:08:04 -0800

    Revert commit

--- games/urls.py
+++ games/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from games.views import HomeView, MathGameView, AnagramGameView, record_score
 
@@ -9,6 +8,5 @@ urlpatterns = [
     path('math-game/', MathGameView.as_view(), name='math-game'),
     path('anagram-game/', AnagramGameView.as_view(), name='anagram-game'),
     #path('games/anagram-hunt/', AnagramGameView.as_view(), name='anagram-game'),
-    path('anagram-hunt/', TemplateView.as_view(template_name="home.html")),
     path('record-score/', record_score, name="record-score")
 ]
--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,20 +1,5 @@
-
-/*
-This config file tells Vue that we don’t want to output to the default directories.
-Instead, we want to specify what goes where.
-This will help us when telling Django where our static files are and where our HTML template has been compiled to.
-*/
-const pages = {
-  index: "src/main.js",
-};
-
 module.exports ={
   publicPath: 'http://localhost:8080', // base URL
-  // publicPath: "/static/vue/",
   outputDir: '../static/dist', // where files will be output
-  // outputDir: "./build/static/vue/",
   indexPath: '../../templates/_base_vue.html', // path for the index file
-  // indexPath: "../../templates/vue_index.html",
-
-  pages: pages,
-};
+}
