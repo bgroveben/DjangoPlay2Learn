9b26c97e7b5d3d78ad2bc112a586fcac22756ab4
bgroveben <bgroveben@gmail.com>
2023-02-08 19:45:20 -0800

    Clean up review tests

--- review/tests.py
+++ review/tests.py
@@ -21,56 +21,33 @@ class ReviewsTest(TestCase):
         # https://docs.djangoproject.com/en/4.1/topics/testing/tools/#django.test.Client.force_login
         login = self.client.force_login(testuser1)
 
-
-    def test_review_form_is_valid(self):
-        # Fill out review form
-        #username = 'testuser1'
-        #username = CustomUser.get_username('admin')
-        username = settings.AUTH_USER_MODEL
-        data={'username': CustomUser.objects.get(username='testuser1'), 'game': 'MATH FACTS', 'votes': "5", 'comment': 'This is a test comment.', 'created': datetime.now(), 'updated': datetime.now()}
-        game = data["game"]
-        votes = data["votes"]
-        comment = data["comment"]
-        #username = data["username"]
-        response = self.client.post('/review/', data)
-        new_review = Review(game=game, votes=votes, comment=comment)
-        new_review_form = ReviewForm(data)
-        self.assertTrue(new_review_form)
-        self.assertEqual(new_review.game, 'MATH FACTS')
-        self.assertEqual(new_review.votes, "5")
-        self.assertEqual(new_review.comment, 'This is a test comment.')
-        #new_review_form.save()
-        #self.assertEqual(new_review.username, 'testuser1')
-        #self.assertTrue(new_review_form.is_valid())
-
-        #new_review.save()
-        #response = self.client.post('/review/', data)
-        #sample.save()
-        #review_form = ReviewForm(sample)
-        #review_form.save()
-        #self.assertTrue(form.is_valid())
-
-
-    def test_review_model(self):
-        sample = Review(username=CustomUser.objects.get(username='testuser1'), game='MATH FACTS', votes=3, comment="This is a test comment", created=datetime.now, updated=datetime.now)
-        sample.save()
-        #print(sample)
-        self.assertTrue(sample)
-        self.assertEqual(str(sample), "MATH FACTS")
-        self.assertEqual(sample.username, CustomUser.objects.get(username='testuser1'))
-        self.assertEqual(sample.game, "MATH FACTS")
-        self.assertEqual(sample.votes, 3)
-        self.assertEqual(sample.comment, "This is a test comment")
-        self.assertTrue(sample.created)
-        self.assertTrue(sample.updated)
-
-    def test_uses_review_template(self):
+    def test_uses_review_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
         self.assertTemplateUsed(response, '_base.html')
 
-    def test_user_can_GET_page(self):
+    def test_user_can_GET_review_home_page(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_can_fill_out_review_form(self):
+        review = Review(game="MATH", votes=5, comment="This is a test comment.")
+        review_form = ReviewForm(review)
+        self.assertTrue(review_form)
+        self.assertIsInstance(review_form, ReviewForm)
+
+    def test_user_can_GET_review_page(self):
+        data={'username': CustomUser.objects.get(username='testuser1').id, 'game': 'MATH FACTS', 'votes': 5, 'comment': 'This is a test comment.', 'created': datetime.now(), 'updated': datetime.now()}
+        game = data["game"]
+        votes = data["votes"]
+        comment = data["comment"]
+        response = self.client.get('/review/')
+        self.assertContains(response, "game")
+        self.assertContains(response, game)
+        self.assertContains(response, "votes")
+        self.assertContains(response, votes)
+        self.assertContains(response, "comment")
+        #self.assertContains(response, comment)
         self.assertEqual(response.status_code, 200)
 
     def test_user_can_submit_review_form(self):
@@ -89,20 +66,45 @@ class ReviewsTest(TestCase):
         self.assertTrue(form.data)
         #self.assertTrue(form.is_valid())
 
-    def test_user_can_view_reviews(self):
-        response = self.client.get('/review/review')
-        self.assertEqual(response.status_code, 301)
-
-
     def test_review_form_field_labels(self):
         username = 'admin'
         data={'username': username, 'game': 'MATH', 'votes': 5, 'comment': 'This is a test comment.', 'created': datetime.now(), 'updated': datetime.now()}
         response = self.client.post('/review/', data)
         form = ReviewForm(data=data)
-        #self.assertTrue(form.is_valid())
         self.assertTrue(form.fields['game'])
         self.assertTrue(form.fields['votes'])
         self.assertTrue(form.fields['comment'])
+
+    def test_user_can_POST_review_using_form(self):
+        data={'username': CustomUser.objects.get(username='testuser1').id, 'game': 'MATH FACTS', 'votes': 5, 'comment': 'This is a test comment.', 'created': datetime.now(), 'updated': datetime.now()}
+        response = self.client.post('/review/', data=data)
+        self.assertTrue(response)
+        self.assertEqual(Review.objects.count(), 1)
+        self.assertRedirects(response, '/review/review/')
+        self.assertEqual(response.status_code, 302)
+        game = data["game"]
+        votes = data["votes"]
+        comment = data["comment"]
+        new_review = Review(game=game, votes=votes, comment=comment)
+        new_review_form = ReviewForm(data)
+        self.assertTrue(new_review_form)
+        self.assertEqual(new_review.game, 'MATH FACTS')
+        self.assertEqual(new_review.votes, 5)
+        self.assertEqual(new_review.comment, 'This is a test comment.')
+        self.assertTrue(new_review_form.is_valid())
+
+    def test_review_model(self):
+        sample = Review(username=CustomUser.objects.get(username='testuser1'), game='MATH FACTS', votes=3, comment="This is a test comment", created=datetime.now, updated=datetime.now)
+        sample.save()
+        #print(sample)
+        self.assertTrue(sample)
+        self.assertEqual(str(sample), "MATH FACTS")
+        self.assertEqual(sample.username, CustomUser.objects.get(username='testuser1'))
+        self.assertEqual(sample.game, "MATH FACTS")
+        self.assertEqual(sample.votes, 3)
+        self.assertEqual(sample.comment, "This is a test comment")
+        self.assertTrue(sample.created)
+        self.assertTrue(sample.updated)
 
     def test_review_meta_fields(self):
         review = Review(game="MATH", votes=5, comment="This is a test comment.")
@@ -115,7 +117,3 @@ class ReviewsTest(TestCase):
         game_choices = review._meta.get_field('game').choices
         self.assertEqual(game_choices, [('MATH FACTS', 'Math Facts'), ('ANAGRAM HUNT', 'Anagram Hunt')])
 
-    def test_user_can_fill_out_review_form(self):
-        review = Review(game="MATH", votes=5, comment="This is a test comment.")
-        review_form = ReviewForm(review)
-        self.assertTrue(review_form)
